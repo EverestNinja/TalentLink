6ebe4e87d00496311f6773a750c82f1a6fec3882
sushil chaulagain <sushilchlgn@gmail.com>
2025-06-11 18:52:35 +0545

    Remove comment

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -16,13 +16,6 @@ function Contact() {
         <div className="absolute inset-0 bg-gradient-to-r from-[#b263fc] to-[#8928e2]" />
         {/* Background image with low opacity */}
         <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80')] bg-cover bg-center opacity-30" />
-        {/* Optional decorative image */}
-        {/* <img
-          src="https://storyset.com/illustration/creativity/bro"
-          alt="Creative background"
-          className="absolute inset-0 w-full h-full object-cover opacity-10 select-none pointer-events-none"
-          loading="lazy"
-        /> */}
       </div>
 
       {/* Content */}
