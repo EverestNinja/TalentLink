f6e61299fd9acbea531e67f13f6bbe29f411a0ed
Rabi Roy <rrabir9815@gmail.com>
2025-06-12 09:54:56 +0545

    fix

--- src/pages/Jobs/components/JobsGrid.jsx
+++ src/pages/Jobs/components/JobsGrid.jsx
@@ -5,7 +5,7 @@ const JobsGrid = ({ jobs }) => {
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-6">Latest Jobs</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {jobs.length > 0 ? (
           jobs.map((job) => (
             <JobCard key={job.id} job={job} />
